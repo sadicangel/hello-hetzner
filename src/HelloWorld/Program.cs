64dd4f05da14deccb150eee619d709747c52a4f6
sadicangel <sadicangel@hotmail.com>
2024-09-27 19:10:21 +0100

    Adds config to prevent antiforgery from throwing.

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -2,7 +2,7 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDataProtection(opts => opts.ApplicationDiscriminator = "hello.hetzner");
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
